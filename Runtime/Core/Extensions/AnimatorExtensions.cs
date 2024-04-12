edde111b9734bb1b27047dd0fff36c41aedf9f77
Jacob <JacobCohenIsrael@gmail.com>
2024-04-12 21:17:58 +0300

    fix: added min time to wait for animator triggerAndWaitForState helper function

--- Runtime/Core/Extensions/AnimatorExtensions.cs
+++ Runtime/Core/Extensions/AnimatorExtensions.cs
@@ -10,10 +10,10 @@ public static class AnimatorExtensions
         return animator.GetOrAddComponent<AnimatorListener>().WaitForState(animator, stateName, layer, minTimeToWait);
     }
     
-    public static IPromise TriggerAndWaitForState(this Animator animator, string trigger, string stateName, int layer = 0)
+    public static IPromise TriggerAndWaitForState(this Animator animator, string trigger, string stateName, int layer = 0, float minTimeToWait = 0f)
     {
         animator.SetTrigger(trigger);
-        return animator.GetOrAddComponent<AnimatorListener>().WaitForState(animator, stateName, layer).Then(() =>
+        return animator.GetOrAddComponent<AnimatorListener>().WaitForState(animator, stateName, layer, minTimeToWait).Then(() =>
         {
             if (animator != null && animator.gameObject.activeInHierarchy)
                 animator.ResetTrigger(trigger);
